2358fc41c92f758f87cc9704a507c918c5901d56
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-16 13:43:01 +0800

    fix: useSidebarToggle

--- src/hooks/use-sidebar-toggle.ts
+++ src/hooks/use-sidebar-toggle.ts
@@ -9,21 +9,20 @@ interface useSidebarToggleStore {
 export const useSidebarToggle = create(
   persist<useSidebarToggleStore>(
     (set, get) => {
-      const setIsOpen = () => {
-        set({ isOpen: !get().isOpen })
+      const setIsOpen = () => set({ isOpen: !get().isOpen })
+      if (typeof window === "undefined") return { isOpen: false, setIsOpen }
 
-        if (typeof window === "undefined") return { isOpen: false, setIsOpen }
-        const sidebarStateFromStorage = localStorage.getItem("sidebarOpen")
-        return {
-          isOpen: sidebarStateFromStorage
-            ? JSON.parse(sidebarStateFromStorage)?.state?.isOpen
-            : true,
-          setIsOpen
-        }
-      },
-        {
-          name: "sidebarOpen",
-          storage: createJSONStorage(() => localStorage)
+      const sidebarStateFromStorage = localStorage.getItem("sidebarOpen")
+      return {
+        isOpen: sidebarStateFromStorage
+          ? !!JSON.parse(sidebarStateFromStorage)?.state?.isOpen
+          : true,
+        setIsOpen
+      }
+    },
+    {
+      name: "sidebarOpen",
+      storage: createJSONStorage(() => localStorage)
     }
   )
 )
