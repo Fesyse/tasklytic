08235216b1c0a28efc870aeb71b9ac625618313f
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-18 00:57:50 +0800

    fix: sign in middleware

--- src/app/auth/sign-in/_middleware.ts
+++ src/app/auth/sign-in/_middleware.ts
@@ -1,9 +1,9 @@
 import { type MiddlewareFunctionProps } from "@rescale/nemo"
-import { getSession } from "next-auth/react"
 import { NextResponse } from "next/server"
+import { getServerAuthSession } from "@/server/auth"
 
 export async function signInMiddleware({ request }: MiddlewareFunctionProps) {
-  const session = await getSession()
+  const session = await getServerAuthSession()
 
   if (session) return NextResponse.redirect(new URL("/dashboard", request.url))
 
