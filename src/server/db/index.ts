525b47f5767b3efc143a1d9110d6a1e6ecd5f7fd
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-17 23:59:01 +0800

    fix: db connection url

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -13,7 +13,7 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined
 }
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL)
+const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL)
 if (env.NODE_ENV !== "production") globalForDb.conn = conn
 
 export const db =
