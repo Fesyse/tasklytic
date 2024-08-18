582b7b48bacce47fd2ca3d5c1b70f9dc1e57b23d
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-18 22:19:23 +0800

    fix: use auth function instead of deprecated getServerAuthSession

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -9,7 +9,7 @@
 import { TRPCError, initTRPC } from "@trpc/server"
 import superjson from "superjson"
 import { ZodError } from "zod"
-import { getServerAuthSession } from "@/server/auth"
+import { auth } from "@/server/auth"
 import { db } from "@/server/db"
 
 /**
@@ -25,7 +25,7 @@ import { db } from "@/server/db"
  * @see https://trpc.io/docs/server/context
  */
 export const createTRPCContext = async (opts: { headers: Headers }) => {
-  const session = await getServerAuthSession()
+  const session = await auth()
 
   return {
     db,
