a95d0451871adee8c9664a768d899b0d49ef0219
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-21 23:52:55 +0800

    rework home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,42 +26,43 @@ export default function Home() {
           </Balance>
         </section>
 
-        <div className="flex flex-col-reverse md:flex-col">
-          <div className="relative h-full w-full">
-            <Beam className="-mt-20 hidden xl:ml-28 xl:block" />
-            <FeaturesSection />
+        <section className="relative pb-20 pt-8 text-center md:pt-12">
+          <div className="relative z-10">
+            <h2 className="mb-2 text-xl font-bold md:mb-4 md:text-2xl">
+              Ready to boost your productivity?
+            </h2>
+            <p className="mb-4 text-base md:mb-6 md:text-lg">
+              Join Tasklytic today and streamline your task management
+            </p>
+            <Button
+              size="lg"
+              asChild
+              className={cn(
+                "group relative overflow-hidden transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-lg",
+                // light mode
+                "bg-gradient-to-tr from-zinc-900 to-zinc-700 text-zinc-50 hover:shadow-zinc-500/30",
+                // dark mode
+                "dark:bg-gradient-to-tr dark:from-zinc-50 dark:to-zinc-100 dark:text-zinc-900 dark:hover:shadow-zinc-700/30"
+              )}
+            >
+              <Link href="/auth/sign-in">
+                Get Started
+                {/* gives shiny effect on hover */}
+                <span className="absolute inset-0 flex size-full justify-center [transform:skew(-14deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-14deg)_translateX(100%)]">
+                  <span className="relative h-full w-8 bg-white/20 dark:bg-black/10" />
+                </span>
+              </Link>
+            </Button>
           </div>
+        </section>
 
-          <section className="relative pb-20 pt-8 text-center md:pt-12">
-            <div className="relative z-10">
-              <h2 className="mb-2 text-2xl font-bold md:mb-4 md:text-3xl">
-                Ready to boost your productivity?
-              </h2>
-              <p className="mb-4 text-base md:mb-8 md:text-xl">
-                Join Tasklytic today and streamline your task management
-              </p>
-              <Button
-                size="lg"
-                asChild
-                className={cn(
-                  "group relative overflow-hidden transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-lg",
-                  // light mode
-                  "bg-gradient-to-tr from-zinc-900 to-zinc-700 text-zinc-50 hover:shadow-zinc-500/30",
-                  // dark mode
-                  "dark:bg-gradient-to-tr dark:from-zinc-50 dark:to-zinc-100 dark:text-zinc-900 dark:hover:shadow-zinc-700/30"
-                )}
-              >
-                <Link href="/auth/sign-in">
-                  Get Started
-                  {/* gives shiny effect on hover */}
-                  <span className="absolute inset-0 flex size-full justify-center [transform:skew(-14deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-14deg)_translateX(100%)]">
-                    <span className="relative h-full w-8 bg-white/20 dark:bg-black/10" />
-                  </span>
-                </Link>
-              </Button>
-            </div>
-          </section>
-        </div>
+        <section className="relative h-full w-full">
+          <Beam className="-mt-[5.75rem] hidden xl:ml-28 xl:block" />
+          <h2 className="mb-2 text-center text-xl font-bold md:mb-4 md:text-2xl">
+            Our features
+          </h2>
+          <FeaturesSection />
+        </section>
       </div>
     </div>
   )
