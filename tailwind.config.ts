6144a8b711888be169234bd9f9cedef2a3d799a9
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-30 22:11:43 +0800

    feat: add collapsible animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -98,12 +98,22 @@ const config = {
             opacity: "1",
             transform: "translate(-50%,-40%) scale(1)"
           }
+        },
+        "collapsible-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-collapsible-content-height)" }
+        },
+        "collapsible-up": {
+          from: { height: "var(--radix-collapsible-content-height)" },
+          to: { height: "0" }
         }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        spotlight: "spotlight 2s ease .75s 1 forwards"
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+        "collapsible-down": "collapsible-down 0.2s ease-out",
+        "collapsible-up": "collapsible-up 0.2s ease-out"
       }
     }
   },
