9678b8edeee6d8df6b32bba71e16b7b11416bd85
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-30 22:14:56 +0800

    refactor: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import svgToDataUri from "mini-svg-data-uri"
 import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette"
-import type { Config, PluginCreator } from "tailwindcss/types/config"
+import type {
+  Config,
+  KeyValuePair,
+  PluginCreator
+} from "tailwindcss/types/config"
 
+// eslint-disable-next-line @typescript-eslint/unbound-method
 const addVariablesForColors: PluginCreator = ({ addBase, theme }) => {
   const allColors = flattenColorPalette(theme("colors")) as string | string[]
   const newVars = Object.fromEntries(
@@ -11,6 +16,36 @@ const addVariablesForColors: PluginCreator = ({ addBase, theme }) => {
   addBase({
     ":root": newVars
   })
+}
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+const gridBackground: PluginCreator = ({ matchUtilities, theme }) => {
+  matchUtilities(
+    {
+      "bg-grid": value => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`
+      }),
+      "bg-grid-small": value => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`
+      }),
+      "bg-dot": value => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+        )}")`
+      })
+    },
+    {
+      values: flattenColorPalette(theme("backgroundColor")) as KeyValuePair<
+        string,
+        string
+      >,
+      type: "color"
+    }
+  )
 }
 
 const config = {
@@ -120,28 +155,7 @@ const config = {
   plugins: [
     require("tailwindcss-animate"),
     addVariablesForColors,
-    function ({ matchUtilities, theme }: any) {
-      matchUtilities(
-        {
-          "bg-grid": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`
-          }),
-          "bg-grid-small": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`
-          }),
-          "bg-dot": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`
-          })
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      )
-    }
+    gridBackground
   ]
 } satisfies Config
 
