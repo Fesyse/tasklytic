30912ec084c14af517648350730f5f4fa617b4a1
fesyse <maksimlol.mail.com@mail.ru>
2024-12-15 22:16:11 +0800

    fix: fixed toolbar was rendering on settings page

--- src/components/editor/plugins/fixed-toolbar-plugin.tsx
+++ src/components/editor/plugins/fixed-toolbar-plugin.tsx
@@ -2,35 +2,6 @@
 
 import { createPlatePlugin } from "@udecode/plate-common/react"
 
-import { FixedToolbar } from "@/components/plate-ui/fixed-toolbar"
-import { FixedToolbarButtons } from "@/components/plate-ui/fixed-toolbar-buttons"
-import { isCuid } from "@/lib/utils"
-import { usePathname } from "next/navigation"
-
 export const FixedToolbarPlugin = createPlatePlugin({
-  key: "fixed-toolbar",
-  render: {
-    beforeEditable: ToolbarBeforeEditor
-  }
+  key: "fixed-toolbar"
 })
-
-function ToolbarBeforeEditor() {
-  const pathname = usePathname()
-  const splittedPathname = pathname
-    .split("/")
-    .slice(1, pathname.split("/").length)
-
-  if (
-    splittedPathname[0] === "projects" &&
-    (splittedPathname[1] ? isCuid(splittedPathname[1]) : false) &&
-    (splittedPathname[3] ? isCuid(splittedPathname[3]) : false)
-  ) {
-    return null
-  }
-
-  return (
-    <FixedToolbar>
-      <FixedToolbarButtons />
-    </FixedToolbar>
-  )
-}
