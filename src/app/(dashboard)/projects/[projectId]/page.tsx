471f047fbf1d7db1bda530cb60da373aafe99280
fesyse <maksimlol.mail.com@mail.ru>
2024-12-24 16:32:41 +0800

    feat: metadata for project page

--- src/app/(dashboard)/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/projects/[projectId]/page.tsx
@@ -1,4 +1,18 @@
-// type ProjectsProps = { params: Promise<{ id: string }> }
+import { Metadata } from "next"
+import { api } from "@/trpc/server"
+
+type ProjectsProps = { params: Promise<{ projectId: string }> }
+
+export async function generateMetadata({
+  params
+}: ProjectsProps): Promise<Metadata> {
+  const { projectId } = await params
+  const project = await api.projects.getById({ id: projectId })
+
+  return {
+    title: project?.name
+  }
+}
 
 export default async function ProjectPage() {
   return <div></div>
