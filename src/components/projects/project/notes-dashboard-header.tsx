19556a15d8f1121a07fffcba2e640f568cdbb72c
fesyse <maksimlol.mail.com@mail.ru>
2025-01-06 20:57:45 +0800

    setup filter form

--- src/components/projects/project/notes-dashboard-header.tsx
+++ src/components/projects/project/notes-dashboard-header.tsx
@@ -1,6 +1,10 @@
 "use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Search, SortAsc } from "lucide-react"
+import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
+import { Form, FormField, FormItem } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import {
   Select,
@@ -9,9 +13,32 @@ import {
   SelectTrigger,
   SelectValue
 } from "@/components/ui/select"
-import { Search, SortAsc } from "lucide-react"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider
+} from "@/components/ui/tooltip"
+import { TooltipTrigger } from "@/components/plate-ui/tooltip"
+import {
+  type NoteDashboardFilterSchema,
+  noteDashboardFilterSchema,
+  sortByWithLabel
+} from "@/lib/schemas"
 
 export function NotesDashboardHeader() {
+  const form = useForm<NoteDashboardFilterSchema>({
+    mode: "onChange",
+    defaultValues: {
+      order: "asc",
+      sortBy: "updated"
+    },
+    resolver: zodResolver(noteDashboardFilterSchema)
+  })
+
+  const onSubmit = (data: NoteDashboardFilterSchema) => {
+    console.log(data)
+  }
+
   return (
     <div className="flex flex-col space-y-4 md:flex-row md:justify-between md:space-y-0">
       <div className="flex w-full md:w-1/3">
@@ -20,22 +47,70 @@ export function NotesDashboardHeader() {
           <Search className="h-4 w-4" />
         </Button>
       </div>
-      <div className="flex space-x-2">
-        <Select defaultValue="updated">
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Sort by" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="updated">Last Updated</SelectItem>
-            <SelectItem value="created">Created Date</SelectItem>
-            <SelectItem value="alphabetical">Alphabetical</SelectItem>
-          </SelectContent>
-        </Select>
-        <Button>
-          <SortAsc className="h-4 w-4 mr-2" />
-          Sort
-        </Button>
-      </div>
+      <Form {...form}>
+        <form className="flex space-x-2" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            name="sortBy"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <Select
+                  defaultValue={field.value}
+                  onValueChange={field.onChange}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Sort by" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {sortByWithLabel.map(item => (
+                      <SelectItem key={item.value} value={item.value}>
+                        {item.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="order"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Button
+                        type="button"
+                        size="icon"
+                        onClick={() =>
+                          field.onChange(field.value === "asc" ? "desc" : "asc")
+                        }
+                      >
+                        {field.value === "asc" ? (
+                          <SortAsc className="h-4 w-4" />
+                        ) : (
+                          <SortAsc className="h-4 w-4 rotate-180" />
+                        )}
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      {field.value === "asc"
+                        ? "Sort Ascending"
+                        : "Sort Descending"}
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </FormItem>
+            )}
+          />
+
+          <Button>
+            <SortAsc className="h-4 w-4 mr-2" />
+            Sort
+          </Button>
+        </form>
+      </Form>
     </div>
   )
 }
--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,5 +1,5 @@
-import { z } from "zod"
 import { ACCEPTED_IMAGE_TYPES, MAX_FILE_SIZE } from "@/lib/constants"
+import { z } from "zod"
 
 const settingsSchema = z.object({
   sidebar: z.object({
@@ -50,12 +50,21 @@ const blockContent = z.array(
   })
 )
 
+const sortBy = ["updated", "created", "alphabetical"] as const
+const sortByWithLabel = [{ value: "updated", label: "Last Updated" }, { value: "created", label: "Created Date" }, { value: "alphabetical", label: "Alphabetical" }] as const
+const order = ["asc", "desc"] as const
+
+const noteDashboardFilterSchema = z.object({
+  sortBy: z.enum(sortBy),
+  order: z.enum(order)
+})
+
+type NoteDashboardFilterSchema = z.infer<typeof noteDashboardFilterSchema>
+
 export {
   blockContent,
-  createProjectSchema,
-  settingsSchema,
-  updateProjectSchema,
-  type CreateProjectSchema,
-  type SettingsSchema,
+  createProjectSchema, noteDashboardFilterSchema, settingsSchema, sortByWithLabel, updateProjectSchema,
+  type CreateProjectSchema, type NoteDashboardFilterSchema, type SettingsSchema,
   type UpdateProjectSchema
 }
+
