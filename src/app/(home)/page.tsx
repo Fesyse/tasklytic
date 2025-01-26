e661a3da55a17f86ff4621eff29418aaa0c14751
fesyse <maksimlol.mail.com@mail.ru>
2025-01-26 17:52:20 +0800

    feat: better second section, better spotlight

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -14,71 +14,91 @@ import { siteConfig } from "@/config"
 
 export default function Home() {
   return (
-    <div className="min-h-[calc(100vh-var(--dashboard-header-size))]) container relative rounded dark:bg-grid-white/[0.02]">
-      <Spotlight className="hidden dark:block" fill="rgba(255,255,255,0.2)" />
-      <div className="px-4">
-        <section className="mt-4 min-h-[calc(100vh-20rem)] flex flex-col justify-center">
-          <div className="md:container">
-            <div className="grid items-center gap-8 lg:grid-cols-2">
-              <div className="flex flex-col items-center text-center lg:items-start lg:text-left relative">
-                <Badge variant="outline">
-                  {siteConfig.announcement.title}
-                  <ArrowUpRight className="ml-2 size-4" />
-                </Badge>
-                <Beam className="-mt-12 -ml-2 hidden xl:block" />
-                <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl">
-                  {siteConfig.title}
-                </h1>
-                <Balance>
-                  <TextGenerateEffect
-                    words={siteConfig.description}
-                    duration={0.25}
-                    className="!text-sm sm:!text-base md:!text-lg mb-8 max-w-xl text-muted-foreground lg:text-xl"
-                  />
-                </Balance>
-                <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
-                  <Button className="w-full sm:w-auto">
-                    <Link href="/auth/sign-in" prefetch={true}>
-                      Start for Free 🎉
-                    </Link>
-                  </Button>
-                  <Button
-                    variant="outline"
-                    className="w-full sm:w-auto"
-                    asChild
-                  >
-                    <Link href="/pricing">
-                      Pricing Plans
-                      <ArrowUpRight className="ml-2 size-4" />
-                    </Link>
-                  </Button>
-                </div>
-              </div>
-              <Image
-                src="https://shadcnblocks.com/images/block/placeholder-1.svg"
-                width={1280}
-                height={720}
-                alt="placeholder hero"
-                className="max-h-96 w-full rounded-md object-cover"
+    <div className="min-h-[calc(100vh-var(--dashboard-header-size))]) relative rounded dark:bg-grid-white/[0.02]">
+      <Spotlight />
+
+      <section className="container mt-4 min-h-[calc(100vh-20rem)] flex flex-col justify-center">
+        <div className="grid items-center gap-8 lg:grid-cols-2">
+          <div className="flex flex-col items-center text-center lg:items-start lg:text-left relative">
+            <Badge variant="outline">
+              {siteConfig.announcement.title}
+              <ArrowUpRight className="ml-2 size-4" />
+            </Badge>
+            <Beam className="-mt-12 -ml-2 hidden xl:block" />
+            <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl">
+              {siteConfig.title}
+            </h1>
+            <Balance>
+              <TextGenerateEffect
+                words={siteConfig.description}
+                duration={0.25}
+                className="!text-sm sm:!text-base md:!text-lg mb-8 max-w-xl text-muted-foreground lg:text-xl"
               />
+            </Balance>
+            <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
+              <Button className="w-full sm:w-auto">
+                <Link href="/auth/sign-in" prefetch={true}>
+                  Start for Free 🎉
+                </Link>
+              </Button>
+              <Button variant="outline" className="w-full sm:w-auto" asChild>
+                <Link href="/pricing">
+                  Pricing Plans
+                  <ArrowUpRight className="ml-2 size-4" />
+                </Link>
+              </Button>
             </div>
           </div>
-        </section>
+          <Image
+            src="https://shadcnblocks.com/images/block/placeholder-1.svg"
+            width={1280}
+            height={720}
+            alt="placeholder hero"
+            className="max-h-96 w-full rounded-md object-cover"
+          />
+        </div>
+      </section>
 
-        <section className="relative my-10 min-h-[800px] rounded-lg border-2 bg-muted/50 p-2 md:my-20">
-          <Icons.placeholder className="absolute left-1/2 top-1/2 h-1/2 w-1/2 -translate-x-1/2 -translate-y-1/2 text-muted-foreground/25 grayscale" />
-        </section>
+      <section className="bg-muted relative my-10 min-h-[800px] md:my-20">
+        <svg
+          viewBox="0 0 1440 58"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          className="absolute top-0 bg-transparent"
+        >
+          <path
+            d="M-100 58C-100 58 218.416 36.3297 693.5 36.3297C1168.58 36.3297 1487 58 1487 58V-3.8147e-06H-100V58Z"
+            className="fill-background"
+          ></path>
+        </svg>
+        <div className="container rounded-lg overflow-hidden border-2">
+          <Icons.placeholder className=" absolute left-1/2 top-1/2 h-1/2 w-1/2 -translate-x-1/2 -translate-y-1/2 text-muted-foreground/25 grayscale" />
+        </div>
+        <svg
+          viewBox="0 0 1440 58"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          className="absolute bottom-0"
+        >
+          <path
+            transform="rotate(180) translate(-1440, -60)"
+            d="M-100 58C-100 58 218.416 36.3297 693.5 36.3297C1168.58 36.3297 1487 58 1487 58V-3.8147e-06H-100V58Z"
+            className="fill-background"
+          ></path>
+        </svg>
+      </section>
 
-        <section className="relative flex w-full flex-col justify-center">
-          <h2 className="mb-4 text-center text-2xl font-bold md:mb-6 md:text-4xl">
-            Our features
-          </h2>
-          <FeaturesSection />
-        </section>
-        <section>
-          <CTAWithGithub />
-        </section>
-      </div>
+      <section className="container relative flex w-full flex-col justify-center">
+        <h2 className="mb-4 text-center text-2xl font-bold md:mb-6 md:text-4xl">
+          Our features
+        </h2>
+        <FeaturesSection />
+      </section>
+      <section>
+        <CTAWithGithub />
+      </section>
     </div>
   )
 }
--- src/components/ui/spotlight.tsx
+++ src/components/ui/spotlight.tsx
@@ -1,55 +1,128 @@
-import { cn } from "@/lib/utils"
+"use client"
+
+import { motion } from "motion/react"
 
 type SpotlightProps = {
-  className?: string
-  fill?: string
+  gradientFirst?: string
+  gradientSecond?: string
+  gradientThird?: string
+  translateY?: number
+  width?: number
+  height?: number
+  smallWidth?: number
+  duration?: number
+  xOffset?: number
 }
 
-export const Spotlight = ({ className, fill }: SpotlightProps) => {
+export const Spotlight = ({
+  gradientFirst = "radial-gradient(68.54% 68.72% at 55.02% 31.46%, hsla(210, 100%, 85%, .08) 0, hsla(210, 100%, 55%, .02) 50%, hsla(210, 100%, 45%, 0) 80%)",
+  gradientSecond = "radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .06) 0, hsla(210, 100%, 55%, .02) 80%, transparent 100%)",
+  gradientThird = "radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .04) 0, hsla(210, 100%, 45%, .02) 80%, transparent 100%)",
+  translateY = -350,
+  width = 560,
+  height = 1380,
+  smallWidth = 240,
+  duration = 7,
+  xOffset = 100
+}: SpotlightProps = {}) => {
   return (
-    <svg
-      className={cn(
-        "pointer-events-none absolute z-[2] h-[169%] w-[138%] animate-spotlight opacity-0 lg:w-[84%]",
-        className
-      )}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 3787 2842"
-      fill="none"
+    <motion.div
+      initial={{
+        opacity: 0
+      }}
+      animate={{
+        opacity: 1
+      }}
+      transition={{
+        duration: 1.5
+      }}
+      className="pointer-events-none absolute inset-0 h-full w-full"
     >
-      <g filter="url(#filter)">
-        <ellipse
-          cx="1924.71"
-          cy="273.501"
-          rx="1924.71"
-          ry="273.501"
-          transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
-          fill={fill ?? "white"}
-          fillOpacity="0.21"
-        ></ellipse>
-      </g>
-      <defs>
-        <filter
-          id="filter"
-          x="0.860352"
-          y="0.838989"
-          width="3785.16"
-          height="2840.26"
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          ></feBlend>
-          <feGaussianBlur
-            stdDeviation="151"
-            result="effect1_foregroundBlur_1065_8"
-          ></feGaussianBlur>
-        </filter>
-      </defs>
-    </svg>
+      <motion.div
+        animate={{
+          x: [0, xOffset, 0]
+        }}
+        transition={{
+          duration,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut"
+        }}
+        className="absolute top-0 left-0 w-screen h-screen z-40 pointer-events-none"
+      >
+        <div
+          style={{
+            transform: `translateY(${translateY}px) rotate(-45deg)`,
+            background: gradientFirst,
+            width: `${width}px`,
+            height: `${height}px`
+          }}
+          className={`absolute top-0 left-0`}
+        />
+
+        <div
+          style={{
+            transform: "rotate(-45deg) translate(5%, -50%)",
+            background: gradientSecond,
+            width: `${smallWidth}px`,
+            height: `${height}px`
+          }}
+          className={`absolute top-0 left-0 origin-top-left`}
+        />
+
+        <div
+          style={{
+            transform: "rotate(-45deg) translate(-180%, -70%)",
+            background: gradientThird,
+            width: `${smallWidth}px`,
+            height: `${height}px`
+          }}
+          className={`absolute top-0 left-0 origin-top-left`}
+        />
+      </motion.div>
+
+      <motion.div
+        animate={{
+          x: [0, -xOffset, 0]
+        }}
+        transition={{
+          duration,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut"
+        }}
+        className="absolute top-0 right-0 w-screen h-screen z-40 pointer-events-none"
+      >
+        <div
+          style={{
+            transform: `translateY(${translateY}px) rotate(45deg)`,
+            background: gradientFirst,
+            width: `${width}px`,
+            height: `${height}px`
+          }}
+          className={`absolute top-0 right-0`}
+        />
+
+        <div
+          style={{
+            transform: "rotate(45deg) translate(-5%, -50%)",
+            background: gradientSecond,
+            width: `${smallWidth}px`,
+            height: `${height}px`
+          }}
+          className={`absolute top-0 right-0 origin-top-right`}
+        />
+
+        <div
+          style={{
+            transform: "rotate(45deg) translate(180%, -70%)",
+            background: gradientThird,
+            width: `${smallWidth}px`,
+            height: `${height}px`
+          }}
+          className={`absolute top-0 right-0 origin-top-rightl`}
+        />
+      </motion.div>
+    </motion.div>
   )
 }
