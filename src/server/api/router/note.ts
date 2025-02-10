c443b5fabbc5fa8c2e56b386f44fa44995e0f4aa
fesyse <maksimlol.mail.com@mail.ru>
2025-02-10 17:03:06 +0800

    full moved caching to next.js 'use cache'

--- src/server/api/router/note.ts
+++ src/server/api/router/note.ts
@@ -1,8 +1,14 @@
 import { asc, desc, eq, like, or } from "drizzle-orm"
+import { revalidateTag } from "next/cache"
+import { cacheTag } from "next/dist/server/use-cache/cache-tag"
 import { z } from "zod"
 import { searchQueryFormat } from "@/lib/utils"
 import { blockContent, order, sortBy } from "@/lib/schemas"
-import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc"
+import {
+  createTRPCRouter,
+  ProtectedCtx,
+  protectedProcedure
+} from "@/server/api/trpc"
 import { blocks, createCuid, notes, type Note } from "@/server/db/schema"
 
 const cacheKeys = {
@@ -10,6 +16,201 @@ const cacheKeys = {
   one: `projects:notes:one`,
   pinned: `projects:notes:all:pinned`,
   unpinned: `projects:notes:all:unpinned`
+}
+
+const filtersSchema = z
+  .object({
+    sortBy: z.enum(sortBy).optional(),
+    order: z.enum(order).optional(),
+    search: z.string().optional()
+  })
+  .optional()
+
+const getNoteById = async (
+  data: { projectId: string; id: string },
+  ctx: ProtectedCtx
+) => {
+  "use cache"
+  const note = await ctx.db.query.notes.findFirst({
+    where: (notesTable, { and, not, eq }) =>
+      and(
+        eq(notesTable.id, data.id),
+        or(
+          // if user is owner of the note, then we showing private note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            not(eq(notesTable.userId, ctx.session.user.id)),
+            eq(notesTable.private, true)
+          ),
+          // otherwise we only showing public note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            eq(notesTable.private, false)
+          )
+        )
+      )
+  })
+
+  cacheTag(`${cacheKeys.one}:${data.projectId}:${data.id}`)
+
+  return note
+}
+
+const getAllNotes = async (
+  data: {
+    projectId: string
+    filters: z.infer<typeof filtersSchema>
+  },
+  ctx: ProtectedCtx
+) => {
+  "use cache"
+  const columnSortBy = data.filters
+    ? data.filters.sortBy !== "alphabetical"
+      ? data.filters.sortBy
+      : "title"
+    : undefined
+
+  const result: Note[] = await ctx.db.query.notes.findMany({
+    orderBy: data.filters
+      ? [
+          data.filters.order === "desc"
+            ? desc(notes[columnSortBy ?? "title"])
+            : asc(notes[columnSortBy ?? "title"])
+        ]
+      : undefined,
+    where: (notesTable, { and, not, eq }) =>
+      and(
+        data.filters?.search
+          ? like(notesTable.title, searchQueryFormat(data.filters.search))
+          : undefined,
+        or(
+          // if user is owner of the note, then we showing private note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            not(eq(notesTable.userId, ctx.session.user.id)),
+            eq(notesTable.private, true)
+          ),
+          // otherwise we only showing public note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            eq(notesTable.private, false)
+          )
+        )
+      )
+  })
+
+  cacheTag(`${cacheKeys.all}:${data.projectId}`)
+
+  return result
+}
+
+// Pinned/Unpinned
+
+const getAllPinnedNotes = async (
+  data: {
+    projectId: string
+  },
+  ctx: ProtectedCtx
+) => {
+  "use  cache"
+  const result: Note[] = await ctx.db.query.notes.findMany({
+    where: (notesTable, { and, not, eq }) =>
+      and(
+        or(
+          // if user is owner of the note, then we showing private note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            not(eq(notesTable.userId, ctx.session.user.id)),
+            eq(notesTable.private, true)
+          ),
+          // otherwise we only showing public note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            eq(notesTable.private, false)
+          )
+        ),
+        eq(notesTable.isPinned, true)
+      )
+  })
+
+  cacheTag(
+    `${cacheKeys.all}:${data.projectId}`,
+    `${cacheKeys.all}:${data.projectId}:pinned`
+  )
+
+  return result
+}
+const getAllUnpinnedNotes = async (
+  data: {
+    projectId: string
+  },
+  ctx: ProtectedCtx
+) => {
+  "use  cache"
+  const result: Note[] = await ctx.db.query.notes.findMany({
+    where: (notesTable, { and, not, eq }) =>
+      and(
+        or(
+          // if user is owner of the note, then we showing private note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            not(eq(notesTable.userId, ctx.session.user.id)),
+            eq(notesTable.private, true)
+          ),
+          // otherwise we only showing public note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            eq(notesTable.private, false)
+          )
+        ),
+        eq(notesTable.isPinned, false)
+      )
+  })
+
+  cacheTag(
+    `${cacheKeys.all}:${data.projectId}`,
+    `${cacheKeys.all}:${data.projectId}:unpinned`
+  )
+
+  return result
+}
+
+// All root notes
+
+const getAllRootNotes = async (
+  data: {
+    projectId: string
+  },
+  ctx: ProtectedCtx
+) => {
+  "use  cache"
+  const result: Note[] = await ctx.db.query.notes.findMany({
+    where: (notesTable, { and, eq, isNull, not }) =>
+      and(
+        eq(notesTable.projectId, data.projectId),
+        or(
+          // if user is owner of the note, then we showing private note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            not(eq(notesTable.userId, ctx.session.user.id))
+          ),
+          // otherwise we only showing public note
+          and(
+            eq(notesTable.projectId, data.projectId),
+            eq(notesTable.private, false)
+          )
+        ),
+        // that way we get notes without a folder
+        isNull(notesTable.folderId)
+      )
+  })
+
+  cacheTag(
+    `${cacheKeys.all}:${data.projectId}`,
+    `${cacheKeys.all}:${data.projectId}:root`
+  )
+
+  return result
 }
 
 export const notesRouter = createTRPCRouter({
@@ -21,73 +222,24 @@ export const notesRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const result = await ctx.db.query.notes.findFirst({
-        where: (notesTable, { and, not, eq }) =>
-          or(
-            // if user is owner of the note, then we showing private note
-            and(
-              eq(notesTable.projectId, input.projectId),
-              not(eq(notesTable.userId, ctx.session.user.id)),
-              eq(notesTable.private, true)
-            ),
-            // otherwise we only showing public note
-            and(
-              eq(notesTable.projectId, input.projectId),
-              eq(notesTable.private, false)
-            )
-          )
-      })
-
+      const result = getNoteById(input, ctx)
       return result
     }),
   getAll: protectedProcedure
     .input(
       z.object({
         projectId: z.string(),
-        filters: z
-          .object({
-            sortBy: z.enum(sortBy).optional(),
-            order: z.enum(order).optional(),
-            search: z.string().optional()
-          })
-          .optional()
+        filters: filtersSchema
       })
     )
     .query(async ({ ctx, input }) => {
-      const columnSortBy = input.filters
-        ? input.filters.sortBy !== "alphabetical"
-          ? input.filters.sortBy
-          : "title"
-        : undefined
-
-      const result: Note[] = await ctx.db.query.notes.findMany({
-        orderBy: input.filters
-          ? [
-              input.filters.order === "desc"
-                ? desc(notes[columnSortBy ?? "title"])
-                : asc(notes[columnSortBy ?? "title"])
-            ]
-          : undefined,
-        where: (notesTable, { and, not, eq }) =>
-          and(
-            input.filters?.search
-              ? like(notesTable.title, searchQueryFormat(input.filters.search))
-              : undefined,
-            or(
-              // if user is owner of the note, then we showing private note
-              and(
-                eq(notesTable.projectId, input.projectId),
-                not(eq(notesTable.userId, ctx.session.user.id)),
-                eq(notesTable.private, true)
-              ),
-              // otherwise we only showing public note
-              and(
-                eq(notesTable.projectId, input.projectId),
-                eq(notesTable.private, false)
-              )
-            )
-          )
-      })
+      const result = await getAllNotes(
+        {
+          projectId: input.projectId,
+          filters: input.filters
+        },
+        ctx
+      )
 
       return result
     }),
@@ -99,25 +251,7 @@ export const notesRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const result: Note[] = await ctx.db.query.notes.findMany({
-        where: (notesTable, { and, not, eq }) =>
-          and(
-            or(
-              // if user is owner of the note, then we showing private note
-              and(
-                eq(notesTable.projectId, input.projectId),
-                not(eq(notesTable.userId, ctx.session.user.id)),
-                eq(notesTable.private, true)
-              ),
-              // otherwise we only showing public note
-              and(
-                eq(notesTable.projectId, input.projectId),
-                eq(notesTable.private, false)
-              )
-            ),
-            eq(notesTable.isPinned, true)
-          )
-      })
+      const result = await getAllPinnedNotes(input, ctx)
 
       return result
     }),
@@ -128,24 +262,7 @@ export const notesRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const result: Note[] = await ctx.db.query.notes.findMany({
-        where: (notesTable, { and, not, eq }) =>
-          and(
-            or(
-              // if user is owner of the note, then we showing private note
-              and(
-                eq(notesTable.projectId, input.projectId),
-                not(eq(notesTable.userId, ctx.session.user.id))
-              ),
-              // otherwise we only showing public note
-              and(
-                eq(notesTable.projectId, input.projectId),
-                eq(notesTable.private, false)
-              )
-            ),
-            eq(notesTable.isPinned, false)
-          )
-      })
+      const result = await getAllUnpinnedNotes(input, ctx)
 
       return result
     }),
@@ -156,26 +273,7 @@ export const notesRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const notes = await ctx.db.query.notes.findMany({
-        where: (notesTable, { and, eq, isNull, not }) =>
-          and(
-            eq(notesTable.projectId, input.projectId),
-            or(
-              // if user is owner of the note, then we showing private note
-              and(
-                eq(notesTable.projectId, input.projectId),
-                not(eq(notesTable.userId, ctx.session.user.id))
-              ),
-              // otherwise we only showing public note
-              and(
-                eq(notesTable.projectId, input.projectId),
-                eq(notesTable.private, false)
-              )
-            ),
-            // that way we get notes without a folder
-            isNull(notesTable.folderId)
-          )
-      })
+      const notes = await getAllRootNotes(input, ctx)
 
       return notes
     }),
@@ -221,6 +319,8 @@ export const notesRouter = createTRPCRouter({
         })
       }
 
+      revalidateTag(`${cacheKeys.all}:${input.projectId}`)
+
       return result
     }),
   update: protectedProcedure
@@ -241,6 +341,8 @@ export const notesRouter = createTRPCRouter({
         .returning()
         .then(r => r[0]!)
 
+      revalidateTag(`${cacheKeys.all}:${result.projectId}`)
+
       return result
     }),
   delete: protectedProcedure
@@ -255,6 +357,8 @@ export const notesRouter = createTRPCRouter({
         .where(eq(notes.id, input.id))
         .returning()
         .then(r => r[0]!)
+
+      revalidateTag(`${cacheKeys.all}:${result.projectId}`)
 
       return result
     })
