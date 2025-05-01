80ccb21b80acfa2dfa4edaef47c9f9740bc7350f
fesyse <maksimlol.mail.com@mail.ru>
2025-05-01 18:07:28 +0800

    feat: breadcrumbs

---
+++ src/components/breadcrumbs.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+
+const breadcrumbs = {
+  "/dashboard": "Dashboard",
+  "/dashboard/todos": "Todos",
+  "/dashboard/habits": "Habits",
+  "/dashboard/goals": "Goals",
+  "/dashboard/settings": "Settings",
+  "/dashboard/settings/profile": "Profile",
+  "/dashboard/settings/notifications": "Notifications",
+  "/dashboard/settings/security": "Security",
+  "/dashboard/settings/privacy": "Privacy"
+}
+
+export function Breadcrumbs() {
+  const pathname = usePathname()
+
+  return (
+    <h1 className="text-base font-medium">
+      {breadcrumbs[pathname as keyof typeof breadcrumbs]}
+    </h1>
+  )
+}
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,5 +1,6 @@
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import { Breadcrumbs } from "./breadcrumbs"
 
 export function SiteHeader() {
   return (
@@ -10,7 +11,7 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
+        <Breadcrumbs />
       </div>
     </header>
   )
