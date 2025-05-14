09130ed644cf8d463b5f6f7f7e53bb06d9dba5db
fesyse <maksimlol.mail.com@mail.ru>
2025-05-14 13:55:29 +0800

    fix: handle case if authorized user wants to accept invite on '/auth/*' pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,20 @@ export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request)
 
   const pathname = request.nextUrl.pathname
+  const searchParams = request.nextUrl.searchParams
 
   if (sessionCookie) {
-    if (pathname === "/" || pathname.startsWith("/auth")) {
+    if (pathname === "/") {
+      return NextResponse.redirect(new URL("/dashboard", request.url))
+    }
+
+    const invitationId = searchParams.get("invitationId")
+    if (pathname.startsWith("/auth") && invitationId) {
+      return NextResponse.redirect(
+        new URL(`/accept-invitation?id=${invitationId}`, request.url)
+      )
+    }
+    if (pathname.startsWith("/auth") && !invitationId) {
       return NextResponse.redirect(new URL("/dashboard", request.url))
     }
   } else {
