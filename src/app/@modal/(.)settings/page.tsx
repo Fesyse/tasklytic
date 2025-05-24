567f492255426954acf7baa3720026f3bd69a318
fesyse <maksimlol.mail.com@mail.ru>
2025-05-24 10:33:19 +0800

    add title to settings

--- src/app/@modal/(.)settings/page.tsx
+++ src/app/@modal/(.)settings/page.tsx
@@ -1,5 +1,17 @@
 import { SettingsProfile } from "@/components/settings/profile"
+import { Separator } from "@/components/ui/separator"
 
 export default function SettingsInterceptedModal() {
-  return <SettingsProfile />
+  return (
+    <div className="space-y-4">
+      <div className="space-y-1">
+        <h2 className="text-2xl font-bold">Profile</h2>
+        <p className="text-muted-foreground text-sm">
+          Manage your profile information
+        </p>
+      </div>
+      <Separator />
+      <SettingsProfile />
+    </div>
+  )
 }
--- src/app/@modal/(.)settings/security/page.tsx
+++ src/app/@modal/(.)settings/security/page.tsx
@@ -1,5 +1,17 @@
 import { SettingsSecurity } from "@/components/settings/security"
+import { Separator } from "@/components/ui/separator"
 
 export default function SettingsSecurityInterceptedModal() {
-  return <SettingsSecurity />
+  return (
+    <div className="space-y-4">
+      <div className="space-y-1">
+        <h2 className="text-2xl font-bold">Security</h2>
+        <p className="text-muted-foreground text-sm">
+          Manage your security settings
+        </p>
+      </div>
+      <Separator />
+      <SettingsSecurity />
+    </div>
+  )
 }
