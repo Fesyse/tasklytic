83f3a65d8e44435c0c85461f05dadbf9c0e5c09a
fesyse <maksimlol.mail.com@mail.ru>
2025-05-29 19:02:08 +0800

    feat: move inner sidebar trigger's to sidebar

--- src/components/layouts/dashboard/index.tsx
+++ src/components/layouts/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar"
 
-import { InnerSidebarTrigger } from "@/components/layouts/dashboard/inner-sidebar-trigger"
 import { AppSidebar } from "@/components/layouts/dashboard/sidebar"
 import { SidebarInset } from "@/components/ui/sidebar"
 
@@ -8,11 +7,7 @@ export const DashboardLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset className="relative">
-        <InnerSidebarTrigger />
-
-        {children}
-      </SidebarInset>
+      <SidebarInset className="relative">{children}</SidebarInset>
     </SidebarProvider>
   )
 }
